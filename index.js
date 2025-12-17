fccb74454d24223734afcae4deca4e1ea752654c
Priyotush <priyotushpaul40@gmail.com>
2025-12-17 20:17:58 +0600

    initialize firebase admin

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 
+const admin = require("firebase-admin");
+
+const decoded = Buffer.from(process.env.FIREBASE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 
 
