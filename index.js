a8e7145251ad08bc46789d48fe136f6106e03117
Priyotush <priyotushpaul40@gmail.com>
2025-12-17 20:18:47 +0600

    add firebase token verification middleware

--- index.js
+++ index.js
@@ -37,6 +37,21 @@ const client = new MongoClient(uri, {
 
 
 
+const verifyFirebaseToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 
 
