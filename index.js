2882488b12749b1405b8b144790dbb01e87cd71d
Priyotush <priyotushpaul40@gmail.com>
2025-12-17 20:15:44 +0600

    initialize express server

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const app = express();
 const port = process.env.PORT || 3000;
-const crypto = require("crypto");
 
-///FIREBASE SERVICE ACCOUNT 
-const admin = require("firebase-admin");
-const decoded = Buffer.from(process.env.FIREBASE_KEY, "base64").toString(
-  "utf8"
-);
-const serviceAccount = JSON.parse(decoded);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-//middleware
-app.use(express.json());
+// middleware
 app.use(cors());
+app.use(express.json());
 
-/
 app.get("/", (req, res) => {
   res.send("Scholar Stream Server");
 });
